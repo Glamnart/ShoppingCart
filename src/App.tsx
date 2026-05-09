1dfa6c27569e66f6f2b0798f7b23c8e521e70ac5
Glamnart <favournchess192@gmail.com>
2026-05-09 17:55:53 +0100

    feat: adds products catalog

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from './components/Header.tsx'
+import Products from './components/Products.tsx'
 import './App.css'
 
 const App:React.FC = () =>{
-  return <>
+  return <div className="bg-[url('src/assets/headerBackground.webp')] bg-cover bg-no-repeat bg-center bg-black/50 bg-blend-overlay">
     <Header/>
-  </>
+    <Products/>
+  </div>
 }
 
 export default App
